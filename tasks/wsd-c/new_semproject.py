8bd8028442628b878e2bb937c0cd899f2d121196
Vera <veraurcenko7@gmail.com>
2026-03-31 13:25:30 +0200

    Update new_semproject.py

--- tasks/wsd-c/new_semproject.py
+++ tasks/wsd-c/new_semproject.py
@@ -409,7 +409,7 @@ for size in context_sizes:
     main(
         range_str="110001:110101",
         json_file="twwtn-en_human (1).json",
-        model="llama3.1:8b",
+        model="mistral-nemo:12b",
         context_window_size=size,
         dry_run=False,
         verbose=True,
