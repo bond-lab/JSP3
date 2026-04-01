57100a6826506a9b265d937c27a9f5f586d4404f
Vera <veraurcenko7@gmail.com>
2026-04-01 23:37:40 +0200

    Update new_semproject.py

--- tasks/wsd-c/new_semproject.py
+++ tasks/wsd-c/new_semproject.py
@@ -409,7 +409,7 @@ for size in context_sizes:
     main(
         range_str="110001:110101",
         json_file="twwtn-en_human (1).json",
-        model="llama3.1:8b",
+        model="qwen3:14b",
         context_window_size=size,
         dry_run=False,
         verbose=True,
